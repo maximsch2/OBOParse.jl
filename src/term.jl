99146b6d384ca328fd95610496932aac72a77174
Maxim Grechkin <grechkin@cs.washington.edu>
2016-02-15 11:53:05 -0800

    remove slow hash function

--- src/term.jl
+++ src/term.jl
@@ -14,7 +14,6 @@ end
 
 
 Base.isless(term1::Term, term2::Term) = isless(term1.id, term2.id)
-Base.hash(term::Term) = hash(term.id)
 Base.show(io::IO, term::Term) = @printf io "Term(\"%s\", \"%s\")" term.id term.name
 Base.showcompact(io::IO, term::Term) = print(io, term.id)
 
