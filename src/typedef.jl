96c407a523cacf982e0189839c892570cacb8162
Alexey Stukalov <astukalov@gmail.com>
2017-09-25 13:17:11 +0000

    isequal(Typedef,Typedef): import Base not required

--- src/typedef.jl
+++ src/typedef.jl
@@ -5,7 +5,5 @@ struct Typedef
     xref::String
 end
 
-import Base: isequal, ==
-
-isequal(td1::Typedef, td2::Typedef) = td1.id == td2.id
-==(td1::Typedef, td2::Typedef) = isequal(td1, td2)
+Base.isequal(td1::Typedef, td2::Typedef) = td1.id == td2.id
+Base.:(==)(td1::Typedef, td2::Typedef) = isequal(td1, td2)
